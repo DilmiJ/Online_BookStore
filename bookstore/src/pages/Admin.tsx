07a66a32041d210b00eb8fc11c84722df5c2652c
Dilmi Jayanetthi <dilmijayanetthi66@gmail.com>
2025-04-14 08:37:54 +0530

    commented code

--- bookstore/src/pages/Admin.tsx
+++ bookstore/src/pages/Admin.tsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { MdInventory } from 'react-icons/md'; 
 import { FaChartLine } from 'react-icons/fa'; 
-
+// commentesd
 const Admin: React.FC = () => {
   const navigate = useNavigate();
   const [userName, setUserName] = useState('');
